00e50ddb1b9a0a5b0e3292431f463c8af0c62133
cynthia <cynthia.chen@alleninstitute.org>
2024-08-14 14:35:19 -0700

    implemented training on one instance - now have to add looping so it works on multiple experiences, then multiple trajectories, finally multiple worlds

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+import logging
+from datetime import datetime
 
 from generators.sparse_map import  SparseMap
 from model import VIN
@@ -8,11 +10,12 @@ from model import VIN
 
 # generate ONE world
 num_envs = 1
-map_side_len = 16
+map_side_len = 8
 obstacle_percent = 20
 scale = 2
+env_n = 0
 
-env = SparseMap.genMaps(num_envs, map_side_len, obstacle_percent, scale)[0] # 0 = 0 freespace, 1 = obstacle
+env = SparseMap.genMaps(num_envs, map_side_len, obstacle_percent, scale)[env_n] # 0 = 0 freespace, 1 = obstacle
 world = env.genPOMDP() # equivalent to what "gridworld" was, stores all info like T, O, R, etc.
     # i sort of don't like how this is implemented ? only 4 actions, not "in order"
     # indexing is [a, s, s'] instead of [s, a, s']
@@ -32,33 +35,62 @@ ax.plot(env.goal_r, env.goal_c, 'bd')
 action_mapping = ["right", "up", "left", "down", "stay"]
 # start somewhere and move.
 start_state = np.random.randint(len(world.states)) # random start state idx
-
-### if u loop this --> line 42 it will be a random walk simulation 
-action = np.random.choice(world.n_actions) # random action idx
-next_state = np.random.choice(range(world.n_states), p=world.T[action, start_state]) # next state based on action and current state
-observation = np.random.choice(range(len(world.observations)), p=world.O[action, next_state]) # um what are these observations/what do they mean...
-reward = world.R[action, start_state, next_state, observation] 
-
-#visualize that ?
-state_x, state_y = SparseMap.roomIndexToRc(env.grid, start_state)
-ax.plot(state_x, state_y, 'ro')
-state_x, state_y = SparseMap.roomIndexToRc(env.grid, next_state)
-ax.plot(state_x, state_y, 'go')
-print(action, action_mapping[action])
-start_state = next_state
-
-# end at goal
 goal_state = SparseMap.rcToRoomIndex(env.grid, env.goal_r, env.goal_c)
-if start_state == goal_state:
-    print("Reached goal!")
+
+n_traj = 10
+max_steps = 5000
+# also, do this "for world in worlds?"
+trajectories = []
+for traj in range(n_traj):
+    trajectory_time = datetime.now()
+    # should i regenerate the start and goal states each time?
+    current_state = start_state
+    # visualize the world
+    plt.ion()
+    fig, ax = plt.subplots()
+    plt.imshow(env.grid.T, cmap="Greys")
+    ax.plot(env.goal_r, env.goal_c, 'bd')
+    start_x, start_y = SparseMap.roomIndexToRc(env.grid, current_state)
+    ax.plot(start_x, start_y, 'ro')
+
+    memory = []
+    total_steps = 0
+    done = False
+    while done == False:
+        step_time = datetime.now()
+        ### if u loop this --> line 42 it will be a random walk simulation 
+        action = np.random.choice(world.n_actions) # random action idx
+        next_state = np.random.choice(range(world.n_states), p=world.T[action, current_state]) # next state based on action and current state
+        observation = np.random.choice(range(len(world.observations)), p=world.O[action, next_state]) # um what are these observations/what do they mean...
+        reward = world.R[action, current_state, next_state, observation] 
+        # visualize that ?
+        state_x, state_y = SparseMap.roomIndexToRc(env.grid, next_state)
+        ax.plot(state_x, state_y, 'go')
+        # plt.pause(0.01)
+        plt.show()
+        current_state = next_state
+        # end at goal
+        if current_state == goal_state:
+            done = True
+        memory.append({
+            'current_state': current_state,
+            'action': action,
+            'next_state': next_state,
+            'reward': reward,
+            'done': done
+        })
+        total_steps += 1
+    if done == True:
+        print("Success!")
+        # store the trajectory
+        trajectories.append(memory)
+    else:
+        print("Failed :(")
+    
+    print('trajectory', traj, datetime.now() - trajectory_time)
 
 
-# store the state movements i guess
-# as a dict? objedct? need to be able to shufflr so list of dicts for now
-experiences = []
-
-# how to distinguish between seperate trajectories
-# how to shuffle and train
+# initialize the model for training after experience collecting is done
 
 config = {
     "imsize": map_side_len + 2, 
@@ -69,17 +101,20 @@ config = {
     "l_q": 10,
     "k": 20,
 }
+model = VIN(config)
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'])
 
-# create "input_view", which is just VIN representation for the reward... ?
-# would be nice to store this within the world object
+# for each trajectory:
+# create input view
 reward_mapping = -1 * np.ones(env.grid.shape) # -1 for freespace
-reward_mapping[env.goal_r, env.goal_c] = 10 # 10 at goal
+reward_mapping[env.goal_r, env.goal_c] = 10 # 10 at goal, if regenerating goals for each world then i'd need to redo this for each goal/trajectory.
 grid_view = np.reshape(env.grid, (1, 1, env.grid.shape[0], env.grid.shape[1]))
 reward_view = np.reshape(reward_mapping, (1, 1, env.grid.shape[0], env.grid.shape[1]))
 input_view = torch.Tensor(np.concatenate((grid_view, reward_view), axis=1)) # inlc empty 1 dim
+# would be nice to store this/method for this directly in world object
 
-
-model = VIN(config)
+# q-map for each world once.
 r, v = model.process_input(input_view)
 for i in range(config['k'] - 1):
     q = model.eval_q(r, v)
@@ -87,20 +122,17 @@ for i in range(config['k'] - 1):
 
 q = model.eval_q(r, v) 
 
-
-##### CODE AFTER THIS IS NOT DONE YET 
-state_x, state_y = experience['current_state'] 
-q_pred = q[0, :, state_x, state_y] # pull state_x, state_y from experiences['current_state']
+# train/learn for each experience
+experience = trajectories[0][0] # first experience from first trajectory
+state_x, state_y = SparseMap.roomIndexToRc(env.grid, experience['current_state']) # should I directly store states as tuple coords? maybe.
+next_state_x, next_state_y = SparseMap.roomIndexToRc(env.grid, experience['next_state'])
+q_pred, _ = model.get_action(q, state_x, state_y)
 q_target = experience['reward'] # pull experiences from stored actions
-
-
-# do something like this.... now just have to reorganize how the expriences are stored....
 if not experience['done']:
-    q_target = q_target + self.gamma * np.max(self.model.q_values[0, :, experience['next_state'][0], experience['next_state'][1]].detach().numpy())
-q_current = self.model.fc(q_current) # if map to actions, should i do this to all q_vals or just q_current?
-q_current[experience['action']] = q_target # hm this only works with q_current having len = n_actions...
-# self.model.fit(experience['current_state'], q_current, verbose=0)
-pred, output = self.model.fc(self.model.q_values[0, :, state_x, state_y]), q_current
-loss = criterion(pred, output)
+    q_target = q_target + world.discount * np.max(q[0, :, next_state_x, next_state_y].detach().numpy())
+
+q_pred[env_n, experience['action']] = q_target
+loss = criterion(model.get_action(q, state_x, state_y)[1], q_pred)
 loss.backward()
 optimizer.step()
+
